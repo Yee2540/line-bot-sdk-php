08d4e4c385b31020b8406281cf865be998384a83
Yee2540 <stumc1357@gmail.com>
2019-04-15 16:12:34 +0700

    Update echo_bot.php

--- line-bot-sdk-tiny/echo_bot.php
+++ line-bot-sdk-tiny/echo_bot.php
@@ -18,8 +18,8 @@
 
 require_once('./LINEBotTiny.php');
 
-$channelAccessToken = '<your channel access token>';
-$channelSecret = '<your channel secret>';
+$channelAccessToken = '3B46grW0q5HwDhGxosbwEHdXjC4NSC2rxzDC7LXNUNS5X26xFYLl/B0LWy+iG63YI4L6wggv5uwDzPzomMAa6I/srYk7pNuzG7uKo0R5BPZWDE/qbQBWkuP+3B3J2QNJ/RuOoRa4Fr+sYyXx2/JrCgdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '402b40b53fba284bd46a2710b3798181';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
